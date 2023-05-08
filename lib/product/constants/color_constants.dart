5c25232320aee061d2f242af21ab11f005764d87
Adiladil0000 <hesenovadil757@gmail.com>
2023-05-08 22:53:07 +0400

    added firebase

---
+++ lib/product/constants/color_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class ColorsConstants {
+  const ColorsConstants._();
+  static const Color purpleDark = Color(0xff2536A7);
+  static const Color purplePrimary = Color(0xff475AD7);
+}
---
+++ lib/product/utility/exception/custom_exception.dart
@@ -0,0 +1,10 @@
+class FireBaseCustomException implements Exception {
+  FireBaseCustomException(this.description);
+
+  final String description;
+
+  @override
+  String toString() {
+    return '$this $description';
+  }
+}
